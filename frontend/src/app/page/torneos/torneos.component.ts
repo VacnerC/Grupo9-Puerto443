bb7dbe92737c468aadb071cc52af90c448902699
VacnerC <vacner.copa@uab.edu.bo>
2025-08-18 20:11:09 -0400

    Frontend en Angular

---
+++ frontend/src/app/page/torneos/torneos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-torneos',
+  imports: [],
+  templateUrl: './torneos.component.html',
+  styleUrl: './torneos.component.scss'
+})
+export class TorneosComponent {
+
+}
